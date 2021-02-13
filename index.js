dd5e4ab7d2f585a1c4e6d77c5c90d35212f1ab3c
sunanda <sunandasamanta38@gmail.com>
2021-02-13 17:36:41 +0530

    update

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 const schedule = require('node-schedule');
 const twit = require('./twitthis');
 
-const time = new Date('2021-01-31T20:03:26.000+5:30');
-
-//shedule work
-// schedule.scheduleJob(time, ()=>{
-//   console.log('job start running' )
-// })
-
-//time interval
 twit();
-
-// const twohrs = '0 */2 * * *'
-// const twosec = '*/2 * * * * *'
-// const twittime = schedule.scheduleJob('*/10 * * * * *', ()=>{
-//   schedule.scheduleJob(twosec, ()=>{
-//     console.log('df')
-    
-//     twittime.cancel()
-//   })
-  
-//   console.log('wait')
-// })
